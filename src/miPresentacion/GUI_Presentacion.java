3e76f3674e1a901a2154550b1833756f4d78c4a1
Mayra Alejandra Sanchez <91445115+MayraAlejandraSanchez@users.noreply.github.com>
2021-11-25 18:29:30 -0500

    Update GUI_Presentacion.java

--- src/miPresentacion/GUI_Presentacion.java
+++ src/miPresentacion/GUI_Presentacion.java
@@ -127,11 +127,14 @@ public class GUI_Presentacion extends JFrame /*Heredo de JFrame para GUI_present
 
         @Override
         public void mouseClicked(MouseEvent e) {
+             panelDatos.removeAll();
             if (e.getSource()==miHobby & e.getClickCount()==2){
                 image = new ImageIcon(getClass().getResource("/recursos/miH_opt.jpg"));
                 labelImagen.setIcon(image);
                 panelDatos.add(labelImagen);
             }
+            revalidate();
+            repaint();
         }
 
         @Override
